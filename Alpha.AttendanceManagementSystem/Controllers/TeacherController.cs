88cc0795d2d22f1d0fde684f58cfecfc8568d9da
Tushar Goyal <tgoyal@tavisca.in>
2019-09-22 12:02:10 +0530

    justforsync

--- Alpha.AttendanceManagementSystem/Controllers/TeacherController.cs
+++ Alpha.AttendanceManagementSystem/Controllers/TeacherController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Alpha.AttendanceManagementSystem.Services;
 using Alpha.AttendanceManagementSystem.Services.DataTypes;
 using Alpha.AttendanceManagementSystem.Services.TeacherServices;
+using AMS.MiddleLayer.DataTypes;
+using AMS.MiddleLayer.Personna;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,15 +16,39 @@ namespace Alpha.AttendanceManagementSystem.Controllers
     [ApiController]
     public class TeacherController : ControllerBase
     {
-        private readonly ITeacherService _ITeacherService ;
+        private readonly ITeacherService _ITeacherService;
         public TeacherController(ITeacherService TeacherService)
         {
             _ITeacherService = TeacherService;
         }
+
+
         [HttpGet]
         public List<Teacher_IdAndName> Get()
         {
             return _ITeacherService.GetAllTeachers();
         }
+
+        //api/teacher/id
+        [HttpGet("{id}")]
+        public Teacher Get(int id)
+        {
+            return _ITeacherService.GetTeacherById(id);
+        }
+
+        //api/teacher/id/report?teacher=true
+        [HttpGet]
+        public TeacherAttendanceReport Get(int teacherId, [FromQuery] bool teacher = true)
+        {
+            return _ITeacherService.GetTeacherReportById(teacherId);
+        }
+
+        
+        //[HttpPost("{teacherId}/{subject}/{subjectId}/{takeattendance}")]
+        //public void Post(int subjectId, int subjectId)
+        //{
+            
+        //}
+        
     }
 }
